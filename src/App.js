e01373482857b03361354d553ae738f6d96422d1
Iggy <igoririanto@rocketmail.com>
2017-03-10 11:41:24 -0800

    allow input object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 //firebase attributes
@@ -24,40 +23,56 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      text: "",
+      text: '',
+      roomName: '',
+      creator: '',
       firebaseList: {},
       firebaseValuesArray: [],
-      firebaseKeysArray: []
+      firebaseKeysArray: [],
+      firebaseUIDArray: []
     }
   }
-  //currently obsolete
   componentDidMount(){
-    //var bigOne = document.getElementById('bigOne');
-    //on is an event listener that listens to any changes done to firebase
     dbRef.on('value', snap => {
+      this.setState({
+        firebaseUIDArray: Object.keys(snap.val())
+      }, () => console.log('firebaseUIDArray: ', this.state.firebaseUIDArray));
       console.log(snap.val());
       console.log('firebaseKeysArray: ', Object.keys(snap.val()));
       console.log('firebaseRoomNameArray: ', Object.values(snap.val()).map((v) => v.roomName));
       console.log('firebaseCreatorArray: ', Object.values(snap.val()).map((v) => v["creator"]));
     });
   }
+
   handleSubmitText(){
-    var pushedRef = dbRef.push(this.state.text);
+    var pushedRef = dbRef.push({
+      roomName: this.state.roomName,
+      creator: this.state.creator
+    });
     console.log('pushedRef: ', pushedRef.key);
     this.setState({
-      text: ''
+      roomName: '',
+      creator: '',
     });
-    //clears up input after button click
-    ReactDOM.findDOMNode(this.refs.textItem).value = '';
   }
+
   //update state of text
   handleTextState(e){
     this.setState({
       text: e.target.value
     })
   }
+  handleRoomName(e){
+    this.setState({
+      roomName: e.target.value
+    })
+  }
+  handleCreator(e){
+    this.setState({
+      creator: e.target.value
+    })
+  }
   render() {
-
     return (
       <div className="App">
         <div className="App-header">
@@ -68,8 +83,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="text" ref="textItem" onChange={this.handleTextState.bind(this)}></input>
-        <button onClick={this.handleSubmitText.bind(this)}>Add list</button>
+
+        <input type="text" ref="roomNameItem" value={this.state.roomName} placeholder="room name" onChange={this.handleRoomName.bind(this)}></input>
+        <input type="text" ref="creatorItem" value={this.state.creator} placeholder="creator name" onChange={this.handleCreator.bind(this)}></input>
+        <button onClick={this.handleSubmitText.bind(this)}>Add Room</button>
+
         <p>Text: {this.state.text}</p>
         <FirebaseValues firebaseValuesArray={this.state.firebaseValuesArray} />
       </div>
